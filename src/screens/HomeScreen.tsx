98485d1a39f9a945c18eede6cb68f30ef43a2a31
IngenieroAlan <brandon.rod182@gmail.com>
2023-07-10 17:09:21 -0700

    PokemonCard Commit

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,17 +1,47 @@
 import React from 'react'
-import { useNavigation } from '@react-navigation/native'
-import { Text, View, Button } from 'react-native'
+import { Text, View, Image, FlatList, ActivityIndicator } from 'react-native'
+/* import { useNavigation } from '@react-navigation/native' */
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { styles } from '../theme/appTheme';
+import { usePokemonPaginated } from '../hooks/usePokemonPaginated';
+import { FadeInImage } from '../components/FadeInImage';
+import { PokemonCard } from '../components/PokemonCard';
 
 export const HomeScreen = () => {
-    const navigator = useNavigation();
+  const { top } = useSafeAreaInsets();
+  const { simplePokemonList, loadPokemons } = usePokemonPaginated();
+  /* const navigator = useNavigation(); */
   return (
-    <View>
-        <Text>Home Screen</Text>
-        <Button
-        title='Pokemon'
-        onPress={()=>{
-            navigator.navigate("PokemonScreen" as never);
-        }}/>
-    </View>
+    <>
+      <Image
+        source={require('../assets/pokebola.png')}
+        style={styles.pokebolaBg}
+      />
+      <View style={{alignItems: 'center',}}>
+        <FlatList
+          data={simplePokemonList}
+          keyExtractor={(pokemon) => pokemon.id}
+          showsVerticalScrollIndicator={false}
+          numColumns={2}
+          //Header
+          ListHeaderComponent={<Text style={{
+            ...styles.title,
+            ...styles.globalMargin,
+            top: top + 20,
+            marginBottom: top + 20,
+            paddingBottom:10,
+          }}>Pokedex</Text>}
+          renderItem={({ item }) => (<PokemonCard pokemon={item} />)}
+          onEndReached={loadPokemons}
+          onEndReachedThreshold={0.4}
+          ListFooterComponent={
+            <ActivityIndicator
+              style={{ height: 100 }}
+              size={20}
+              color="grey"
+            />}
+        />
+      </View>
+    </>
   )
 }
